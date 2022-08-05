2a844412b90a6cc52f4d4264604b837b2501ce91
Asaf erdman <asaferdman23@gmail.com>
2022-08-05 10:05:25 +0300

    Adding checkbox and remember me string

--- app/src/main/java/com/asfmtk/trainy/CoachLogin.java
+++ app/src/main/java/com/asfmtk/trainy/CoachLogin.java
@@ -3,8 +3,13 @@ package com.asfmtk.trainy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CoachLogin extends AppCompatActivity {
+    Button mLoginButton;
+    EditText mPassword;
+    EditText mEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/asfmtk/trainy/CoachRegister.java
+++ app/src/main/java/com/asfmtk/trainy/CoachRegister.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CoachRegister extends AppCompatActivity {
 
@@ -15,17 +16,17 @@ public class CoachRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coach_register_layout);
-
         coachSignMe = (Button) findViewById(R.id.coach_sign_up);
+         SignUpCoach();
+    }
 
-//        public void SignUpCoach() {
-//            coachSignMe.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent intent = new Intent(CoachRegister.this, )
-//                }
-//            });
-//        }
-
+    public void SignUpCoach() {
+        coachSignMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CoachRegister.this, CoachLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
